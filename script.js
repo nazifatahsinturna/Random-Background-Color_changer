cc2fa6229b3801250473345546813b1411c2ae2c
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-10-19 03:28:45 -0400

    automatic color change added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const changeColorbtn = document.getElementById("changeColorBtn"); //select by id
 const body = document.body //selecting the entire body
 
-changeColorbtn.addEventListener('click', () =>{
+
+function changeColor () {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0'); //generating color code
     body.style.backgroundColor = randomColor;
     //.toString(16) means take first 16 digit convert it to - hexadecimal string
@@ -9,4 +10,8 @@ changeColorbtn.addEventListener('click', () =>{
     //characters long by padding with leading zeros if needed (e.g., "3f" → "00003f").
 
     console.log(Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0'));
-});
+}
+changeColorbtn.addEventListener('click', changeColor);
+
+//for automatic change
+setInterval(changeColor, 3 * 1000);
